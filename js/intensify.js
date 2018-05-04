df55212939b86cfb009c9a07711f18920e8d9d9a
Julien Bellue <julien@bellue.fr>
2018-05-04 09:24:55 +0100

    add download link to gif

--- js/intensify.js
+++ js/intensify.js
@@ -66,11 +66,17 @@ function intensify() {
 		canvas.id = "bitmap";
 		document.getElementById("center").appendChild(canvas);
 	}
+	var intense_link = document.getElementById("intensity_link");
+	if (intense_link == undefined) {
+		intense_link = document.createElement('a');
+		intense_link.id = "intensity_link";
+		document.getElementById("center").appendChild(intense_link);
+	}
 	var intense_gif = document.getElementById("intensity_image");
 	if (intense_gif == undefined) {
 		intense_gif = new Image();
 		intense_gif.id = "intensity_image";
-		document.getElementById("center").appendChild(intense_gif);
+		intense_link.appendChild(intense_gif);
 	}
 	var imgCanvas = document.createElement("canvas");
 	var imgCtx = imgCanvas.getContext("2d");
@@ -101,7 +107,8 @@ function intensify() {
 			font_size: document.getElementById("font_range").valueAsNumber,
 			text: document.getElementById("text").value,
 			text_effect: document.getElementById("text-menu").value,
-			img_output: intense_gif
+			img_output: intense_gif,
+			link_to_image: intense_link
 		}
 		let ret = create_gif(options);
 		if (!ret.success) {
@@ -152,6 +159,8 @@ function create_gif(options) {
 	var data_url = "data:image/gif;base64," + encode64(encoder.stream().getData());
 	
 	options.img_output.src = data_url;
+	options.link_to_image.href = data_url;
+	options.link_to_image.download = "intense.gif";
 	
 	hide_all();
 	options.img_output.width = options.ctx.canvas.width;
